0923be33aa7a45dba63837ac9bb16e594d00d298
Gabriel <162917831+thesomewhatyou@users.noreply.github.com>
2025-09-04 16:48:38 -0400

    Update build_iso.sh

--- build_iso.sh
+++ build_iso.sh
@@ -3,19 +3,15 @@
 # Author: thesomewhatyou (GabrielDPP)
 #
 # This script builds a GabeOS-themed Arch-based ISO using mkarchiso and Calamares
-
 if [[ $EUID -ne 0 ]]; then
+
   echo "You must be root to execute this script"
+
   exit 1
+
 fi
 
-# Set theme variables
-ISO_NAME="GabeOS"
-WORK_DIR="/tmp/gabeos-archiso"
-OUT_DIR="out"
-PROFILE_DIR="."
 
-# Build the ISO with mkarchiso (modern syntax)
-mkarchiso build -v -w "$WORK_DIR" -o "$OUT_DIR" -P "$PROFILE_DIR"
 
-echo "GabeOS ISO build complete! Output is in $OUT_DIR."
+
+mkarchiso -v -w /tmp/ht-archiso -r -P .
